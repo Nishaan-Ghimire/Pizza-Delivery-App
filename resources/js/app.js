b93e745272af5c7975a928a3eee6b19bb0e3fcaa
Nishan <nishanghimire92@gmail.com>
2022-10-10 18:47:00 +0545

    add to cart system

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,38 @@
-console.log('From app js');
+import axios from 'axios';
+import Noty from 'noty';
+
+
+let addToCart = document.querySelectorAll('.add-to-cart');
+let cartCounter = document.getElementById('cartCounter');
+
+function updateCart(pizza){
+axios.post('/update-cart',pizza).then(res=>{
+    cartCounter.innerText = res.data.totalQty;
+    new Noty({ 
+        type: 'success',
+        timeout: 1000,
+        text: 'Item added to the cart',
+        progressBar: false,
+        // layout: 'topCenter'
+    }).show();
+    
+}).catch(err=>{
+    new Noty({ 
+        type: 'error',
+        timeout: 1000,
+        text: 'Something went wrong !!',
+        progressBar: false,
+        // layout: 'topCenter'
+    }).show();
+})
+}
+
+
+
+
+addToCart.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        let pizza = JSON.parse(btn.dataset.pizza);
+        updateCart(pizza);
+    })
+})
--- server.js
+++ server.js
@@ -1,16 +1,70 @@
 //Importing required Modules
+require('dotenv').config(); //for using .env file
 const express = require('express');
 const ejs = require('ejs');
-const expressLayout = require('express-ejs-layouts');
+const expressLayout = require('express-ejs-layouts');  //Setting up ejs layouts
 const path = require('path');
+const mongoose = require('mongoose'); //Connect to mongodb
+const session = require('express-session'); // For using session
+const flash = require('express-flash');  // Create an unique session for each every client
+const MongoDbStore = require('connect-mongo');  //use for storing session in database
+
+
 
 // Initializing express and port
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+
+// Database Connection
+const URI = 'mongodb://localhost/pizza';
+
+mongoose.connect(URI, {
+
+useNewUrlParser: true, 
+
+useUnifiedTopology: true 
+
+}, err => {
+if(err) throw err;
+console.log('Connected to MongoDB!!!')
+});
+
+// Session Store
+
+let mongoStore = MongoDbStore.create({
+    mongoUrl : URI,
+    collection: "sessions"
+});
+
+
+
+
+
+// Session config
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000*60*60*24}
+}))
+
+//Initializing express-flash for generating session Id
+app.use(flash());
+
 // Assets
 app.use(express.static('public'));
+app.use(express.json());
 
+
+// Global middleware
+app.use((req,res,next)=>{
+        res.locals.session = req.session;
+        next();
+}
+)
 
 // set Template Engine
 app.use(expressLayout);
@@ -18,22 +72,10 @@ app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
 
 
-// Routing
-    app.get('/',(req,res)=>{
-    res.render('home');
-    });
-    
-    app.get('/cart',(req,res)=>{
-        res.render('customer/cart');
-    })
+ // Routing
+require('./routes/web')(app);
 
-    app.get('/login',(req,res)=>{
-        res.render('auth/login');
-    })
 
-    app.get('/register',(req,res)=>{
-        res.render('auth/register');
-    })
     
     
 // Listing
